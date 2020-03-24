da55aa5b274433a8cb027a83414b78189356dfb8
zheng_xing <Zheng.Xing@Dell.com>
2020-03-24 14:20:06 +0800

    Add project files.

---
+++ NetMqServer/Program.cs
@@ -0,0 +1,29 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+using System.Threading;
+
+namespace NetMqServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using (var server = new ResponseSocket())
+            {
+                server.Bind("tcp://*:5555");
+                while (true)
+                {
+                    var message = server.ReceiveFrameString();
+                    Console.WriteLine("Received {0}", message);
+                    // processing the request
+                    Thread.Sleep(100);
+                    Console.WriteLine("Sending World");
+                    server.SendFrame("World");
+                }
+            }
+        }
+    }
+}
---
+++ UwpSubscriber/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UwpSubscriber
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public static IList<string> allowableCommandLineArgs
+            = new[] { "TopicA", "TopicB", "All" };
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+        }
+
+        private async void Subscribe()
+        {
+            using (var subSocket = new SubscriberSocket())
+            {
+                subSocket.Options.ReceiveHighWatermark = 1000;
+                subSocket.Connect("tcp://localhost:12345");
+                subSocket.Subscribe("");
+          
+                while (true)
+                {
+                    string messageReceived = subSocket.ReceiveFrameString();
+                    await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal,
+                        () => { textBlockContent.Text += messageReceived + "\n"; });
+                    
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Subscribe();
+        }
+    }
+}
